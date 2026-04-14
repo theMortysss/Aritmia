fef166c1e0c0abe5fe8447c92637e560e2343c1b
techm1nd <kirill.shirobokov.2003@inbox.ru>
2026-04-14 09:37:23 +0400

    test master commit

--- app/src/main/java/my/diplom/aritmia/MainActivity.kt
+++ app/src/main/java/my/diplom/aritmia/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var db: AppDatabase
 
+    // test master
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
