475da0481f91750b075a264b14d8bed08e3183e7
theMortysss <104450955+theMortysss@users.noreply.github.com>
2026-08-24 16:16:23 +0400

    Provide multiclass disease network

--- app/src/main/java/my/diplom/aritmia/di/DatabaseModule.kt
+++ app/src/main/java/my/diplom/aritmia/di/DatabaseModule.kt
@@ -10,6 +10,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import my.diplom.aritmia.data.AppDatabase
+import my.diplom.aritmia.diagnosis.DiseaseNetworkRepository
 import my.diplom.aritmia.nn.NetworkRepository
 import javax.inject.Singleton
 
@@ -25,12 +26,16 @@ object DatabaseModule {
             .addMigrations(AppDatabase.MIGRATION_6_7)
             .build()
 
-    @Provides fun provideUserDao(db: AppDatabase)    = db.userDao()
+    @Provides fun provideUserDao(db: AppDatabase) = db.userDao()
     @Provides fun provideSymptomDao(db: AppDatabase) = db.symptomDao()
-    @Provides fun provideRuleDao(db: AppDatabase)    = db.ruleDao()
+    @Provides fun provideRuleDao(db: AppDatabase) = db.ruleDao()
 
     @Provides
     @Singleton
     fun provideNetworkRepository(@ApplicationContext context: Context): NetworkRepository =
         NetworkRepository(context)
+
+    @Provides
+    @Singleton
+    fun provideDiseaseNetworkRepository(): DiseaseNetworkRepository = DiseaseNetworkRepository()
 }
